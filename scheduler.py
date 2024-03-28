93b92e05ef0b43bf234d00aa659b85a1e930addd
BHANUJ <59148323+bhanuj10@users.noreply.github.com>
2024-03-28 23:06:27 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,19 +1,5 @@
-
-import os,csv
-os.chdir("D:\Subd\schedule")
-"""
-with open("schedule.csv","w") as csv_file:
-    reader_csv = csv.reader(csv_file)
-    reader_csv.writerow()
-"""
-lst=[["apple","apple"],["orange","orange"]]
-
 def addtosched(newtask):
-    f=open("schedule.csv",'w',newline='\n',encoding="utf8")
-    w = csv.writer(f)
-    lst.append(newtask)
-    w.writerows(lst)    
-    f.close()
+    pass
 
 def gettodaysched():
     pass
@@ -25,32 +11,39 @@ def getmissedsched():
     pass
 
 def getsched():
+    pass
 
-    date=list(map(int,input("Enter the date of task (dd-mm-yyyy) : ").split('-')))
-    temp=input("Enter the time of task completion : ")
-    time=list(map(int,temp.split(':'))) if temp!='' else ''
-    return [date,time]
+def history():
+    pass
+
 def menu():
     print("....MY TASK SCHEDULER....")
     print("1. ADD TASK")
     print("2. VIEW TODAY\'S TASKS")
     print("3. VIEW ALL TASKS")
     print("4. VIEW MISSED TASKS")
+    print("5. HISTORY")
+    print("6. EXIT")
     opt = int(input("Enter the option : "))
-    if opt>4 or opt<1:
+    if opt>6 or opt<1:
         print("Invalid OPTION")
         opt=menu()
     return opt
 
 
-if __name__=="1__main__":
-    opt = menu()
-
-    if opt==1:
-        addtosched()
-    elif opt==2:
-        gettodaysched()
-    elif opt==3:
-        getallsched()
-    else:
-        getmissedsched()
+if __name__=="__main__":
+    
+    while 1:
+        opt = menu()
+        if opt==1:
+            addtosched()
+        elif opt==2:
+            gettodaysched()
+        elif opt==3:
+            getallsched()
+        elif opt==4:
+            getmissedsched()
+        elif opt==5:
+            history()
+        else:
+            break
